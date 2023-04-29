aaea7c113430ebd750ed023dc6dd0151d37c09e8
ThiagoDbraga <thiagomrkt@gmail.com>
2023-04-29 18:01:21 -0300

    add-comentarios

--- script.js
+++ script.js
@@ -19,15 +19,18 @@ const initialBoard = [
   [".", ".", ".", ".", "."],
 ];
 
-
 function allocateShips(shipPositions, initialBoard) {
 
   for (let i = 0; i <= shipPositions.length - 1; i++) {
+
+    //Esse codigo esta posicionando os navios S na posiçao de shipPositions
     initialBoard[shipPositions[i][0]][shipPositions[i][1]] = "S";
   }
+
   return initialBoard;
 }
 allocateShips(shipPositions, initialBoard);
+
 
 const guesses = [];
 const mountedBoard = []
@@ -36,6 +39,7 @@ function checkGuesses(guesses, mountedBoard) {
 
   for (let i = 0; i <= guesses.length - 1; i++) {
 
+    //o codigo esta verificando se tem navio S na posiçao do guesses se tiver troca por navio X
     if (mountedBoard[guesses[i][0]][guesses[i][1]] === "S") {
 
       mountedBoard[guesses[i][0]][guesses[i][1]] = "X"
@@ -49,6 +53,7 @@ function checkGuesses(guesses, mountedBoard) {
 }
 checkGuesses(guesses, mountedBoard);
 
+
 const guessedBoard = []
 
 function checkWinCondition(guessedBoard) {
@@ -58,7 +63,9 @@ function checkWinCondition(guessedBoard) {
   for (let i = 0; i <= guessedBoard.length - 1; i++) {
 
     for (let j = 0; j <= guessedBoard[i].length; j++) {
-
+      
+      //os ifs estao percorrendo todo o tabuleiro e fazendo contagens dos navios (s) e (x)
+      //objetivo da contagem e para saber qual time ganhou
       if (guessedBoard[i][j] === "S") {
         teamS += 1
 
@@ -68,7 +75,8 @@ function checkWinCondition(guessedBoard) {
       }
     }
   }
-
+  
+  //se time S ganha me retorna false se o X ganha me retorna true
   if (teamS > teamX) {
     return false
   } else {
